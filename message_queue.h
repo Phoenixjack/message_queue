ed7fdb1e39d6bc7719aec589cf43de32b61a9ea4
PhoenixJack <35715826+Phoenixjack@users.noreply.github.com>
2026-06-14 15:26:52 -0400

    Refactor message queue class and update structure

--- message_queue.h
+++ message_queue.h
@@ -1,50 +1,88 @@
-#include "Arduino.h"
-#include <stdint.h>
+#pragma once
 
-class PhoenixJack_message_queue {
+#include <Arduino.h>
+#include <stdint.h>
+#include <string.h>
+
+class MessageQueue {
 private:
-  uint8_t _max_messages = 20;
-  uint8_t _num_messages_unsent = 0;
-  uint8_t _msg_chars = 50;
-  uint8_t _oldest_msg_id = 0;
-  uint8_t _newest_msg_id = 0;
-  struct _message_queue {
-    unsigned long timestamp = 0;
-    uint8_t flag = 0;
-    char msg[100];
+  static const uint8_t MAX_MESSAGES = 10;
+  static const uint8_t MAX_MESSAGE_CHARS = 50;
+
+  struct QueueMessage {
+    unsigned long timestamp;
+    uint8_t flag;
+    char msg[MAX_MESSAGE_CHARS];
   };
+
+  QueueMessage _messages[MAX_MESSAGES];
+
+  uint8_t _oldest = 0;
+  uint8_t _newest = 0;
+  uint8_t _count = 0;
+
 public:
-  explicit PhoenixJack_message_queue(uint8_t num_messages, uint8_t msg_chars);  // initialize
-  bool anything_in_queue();
-  bool is_queue_empty();
-  bool is_queue_full();
-  uint8_t queue_length();
-  uint8_t oldest_msg_id();
-  uint8_t newest_msg_id();
-  char* oldest_msg();
-};
-PhoenixJack_message_queue::PhoenixJack_message_queue(uint8_t num_messages = 10, uint8_t msg_chars = 50) {
-  //uint32_t size_needed = num_messages * (msg_chars + 4 + 1);  // calculating how much space this will take; we add a few bytes for the millis() timestamp and a byte for acknowledged
-  _message_queue[_max_messages];
-};
-bool PhoenixJack_message_queue::anything_in_queue() {
-  return (_num_messages_unsent > 0);
-};
-bool PhoenixJack_message_queue::is_queue_empty() {
-  return !anything_in_queue();
-};
-bool PhoenixJack_message_queue::is_queue_full() {
-  return (_num_messages_unsent == _max_messages);
-};
-uint8_t PhoenixJack_message_queue::queue_length() {
-  return _num_messages_unsent;
-};
-uint8_t PhoenixJack_message_queue::oldest_msg_id() {
-  return _oldest_msg_id;
-};
-uint8_t PhoenixJack_message_queue::newest_msg_id() {
-  return _newest_msg_id;
-};
-char* PhoenixJack_message_queue::oldest_msg() {
-  return *_message_queue[_oldest_msg_id];
+  bool is_empty() const {
+    return _count == 0;
+  }
+
+  bool is_full() const {
+    return _count >= MAX_MESSAGES;
+  }
+
+  uint8_t queue_length() const {
+    return _count;
+  }
+
+  bool push(const char* message, uint8_t flag = 0) {
+    if (is_full() || message == nullptr) {
+      return false;
+    }
+
+    _messages[_newest].timestamp = millis();
+    _messages[_newest].flag = flag;
+
+    strncpy(_messages[_newest].msg, message, MAX_MESSAGE_CHARS - 1);
+    _messages[_newest].msg[MAX_MESSAGE_CHARS - 1] = '\0';
+
+    _newest = (_newest + 1) % MAX_MESSAGES;
+    _count++;
+
+    return true;
+  }
+
+  bool pop() {
+    if (is_empty()) {
+      return false;
+    }
+
+    _oldest = (_oldest + 1) % MAX_MESSAGES;
+    _count--;
+
+    return true;
+  }
+
+  const char* oldest_msg() const {
+    if (is_empty()) {
+      return nullptr;
+    }
+
+    return _messages[_oldest].msg;
+  }
+
+  unsigned long oldest_timestamp() const {
+    if (is_empty()) {
+      return 0;
+    }
+
+    return _messages[_oldest].timestamp;
+  }
+
+  uint8_t oldest_flag() const {
+    if (is_empty()) {
+      return 0;
+    }
+
+    return _messages[_oldest].flag;
+  }
 };
